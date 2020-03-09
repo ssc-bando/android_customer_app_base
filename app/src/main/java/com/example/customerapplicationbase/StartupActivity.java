9dd06094c1fea3411b1e07bb95f6bb38e03cdfe3
ssc-bando <satoshi_bando@solver.co.jp>
2020-03-09 15:24:24 +0900

    change: add StartupActivity and set to startup window

---
+++ app/src/main/java/com/example/customerapplicationbase/StartupActivity.java
@@ -0,0 +1,14 @@
+package com.example.customerapplicationbase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StartupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_startup);
+    }
+}
