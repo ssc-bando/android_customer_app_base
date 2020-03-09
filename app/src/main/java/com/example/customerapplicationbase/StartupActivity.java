1cad118d5092ce7a15f6b9ed23f4d7760cb8b312
ssc-bando <satoshi_bando@solver.co.jp>
2020-03-09 15:31:18 +0900

    add: change to MainActivity from button

--- app/src/main/java/com/example/customerapplicationbase/StartupActivity.java
+++ app/src/main/java/com/example/customerapplicationbase/StartupActivity.java
@@ -2,7 +2,12 @@ package com.example.customerapplicationbase;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.example.customerapplicationbase.ui.home.HomeFragment;
 
 public class StartupActivity extends AppCompatActivity {
 
@@ -10,5 +15,15 @@ public class StartupActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_startup);
+    }
+
+    public void onTestExistedUserButtonClicked(View view) {
+        Log.d("TAG", "test:existed user");
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void onTestNewUserButtonClicked(View view) {
+        Log.d("TAG", "test:new user");
     }
 }
