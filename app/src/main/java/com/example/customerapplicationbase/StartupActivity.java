893f7d269715d8f69c878d852b857e79f19f067d
ssc-bando <satoshi_bando@solver.co.jp>
2020-03-09 16:31:04 +0900

    add: new Activity for base of RegisterProgress

--- app/src/main/java/com/example/customerapplicationbase/StartupActivity.java
+++ app/src/main/java/com/example/customerapplicationbase/StartupActivity.java
@@ -8,6 +8,8 @@ import android.util.Log;
 import android.view.View;
 
 import com.example.customerapplicationbase.ui.home.HomeFragment;
+import com.example.customerapplicationbase.ui.register.RegsiterProgressActivity;
+import com.example.customerapplicationbase.ui.register.ShowPermissionFragment;
 
 public class StartupActivity extends AppCompatActivity {
 
@@ -19,12 +21,13 @@ public class StartupActivity extends AppCompatActivity {
 
     public void onTestExistedUserButtonClicked(View view) {
         Log.d("TAG", "test:existed user");
-        //Intent intent = new Intent(this, MainActivity.class);
         Intent intent = new Intent(this, LoggingInActivity.class);
         startActivity(intent);
     }
 
     public void onTestNewUserButtonClicked(View view) {
         Log.d("TAG", "test:new user");
+        Intent intent = new Intent(this, RegsiterProgressActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/customerapplicationbase/ui/register/RegsiterProgressActivity.java
@@ -0,0 +1,16 @@
+package com.example.customerapplicationbase.ui.register;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.customerapplicationbase.R;
+
+public class RegsiterProgressActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_regsiter_progress);
+    }
+}
